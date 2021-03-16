6619f5c6f5a238318262e974300c1ddf15b73d27
chenqihang <chenqihang@imdada.cn>
2021-03-16 19:13:34 +0800

    init

--- init_install_software.sh
+++ init_install_software.sh
@@ -7,7 +7,7 @@ echo '++++安装开发环境软件++++'
 sudo apt install vim tree git golang-go npm net-tools docker.io nodejs python3-pip zsh fish gcc g++ -y
 
 echo '++++开始安装办公软件++++'
-sudo apt install deepin-wine5 deepin.com.wechat Searchmonkey
+sudo apt install deepin-wine5 Searchmonkey
 
 echo '++++开始下载vscode++++'
 wget https://az764295.vo.msecnd.net/stable/2b9aebd5354a3629c3aba0a5f5df49f43d6689f8/code_1.54.3-1615806378_amd64.deb
